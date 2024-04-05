db23af6b01453d90c624da1c2901d4ee4ffc547f
leanhtu1808 <kinglat18082003@gmail.com>
2024-04-05 08:17:58 +0700

    chore: fill more word

---
+++ utils/word_dict.py
@@ -0,0 +1,50 @@
+replace_list = {
+    'ô kêi': 'ok', 'okie': 'ok', 'o kê': 'ok', 'okey': 'ok', 'ôkê': 'ok', 'oki': 'ok', 'oke': 'ok', 'okay': 'ok', 'okê': 'ok',
+    'tks': 'cảm ơn', 'thks': 'cảm ơn', 'thanks': 'cảm ơn', 'ths': 'cảm ơn', 'thank': 'cảm ơn',
+    'kg': 'không', 'not': 'không', 'k': 'không', 'kh': 'không', 'kô': 'không', 'hok': 'không', 'ko': 'không', 'khong': 'không', 'kp': 'không phải',
+    'he he': 'tích cực', 'hehe': 'tích cực', 'hihi': 'tích cực', 'haha': 'tích cực', 'hjhj': 'tích cực', 'thick': 'tích cực',
+    'cc': 'tiêu cực', 'huhu': 'tiêu cực', 'cute': 'dễ thương',
+    'sz': 'cỡ', 'size': 'cỡ', 'dm':'tiêu cực','dme':'tiêu cực','ditme':'tiêu cực',
+    'wa': 'quá', 'wá': 'quá', 'qá': 'quá', 'cx':'cũng','lác': 'tiêu cực',
+    'đx': 'được', 'dk': 'được', 'dc': 'được', 'đk': 'được', 'đc': 'được', 
+    'vs': 'với', 'j': 'gì', '“': ' ', 'time': 'thời gian', 'm': 'mình', 'mik': 'mình', 'r': 'rồi', 'bjo': 'bao giờ', 'very': 'rất',
+    'authentic': 'chuẩn chính hãng', 'aut': 'chuẩn chính hãng', 'auth': 'chuẩn chính hãng', 'date': 'hạn sử dụng', 'hsd': 'hạn sử dụng', 
+    'store': 'cửa hàng', 'sop': 'cửa hàng', 'shopE': 'cửa hàng', 'shop': 'cửa hàng', 
+    'sp': 'sản phẩm', 'product': 'sản phẩm', 'hàg': 'hàng', 
+    'ship': 'giao hàng', 'delivery': 'giao hàng', 'síp': 'giao hàng', 'order': 'đặt hàng',
+
+    'gud': 'tốt', 'wel done': 'tốt', 'good': 'tốt', 'gút': 'tốt', 'tot': 'tốt', 'nice': 'tốt', 'perfect': 'rất tốt', 
+    'quality': 'chất lượng', 'chất lg': 'chất lượng', 'chat': 'chất', 'excelent': 'hoàn hảo', 'bt': 'bình thường',
+    'sad': 'tệ', 'por': 'tệ', 'poor': 'tệ', 'bad': 'tệ', 
+    'beautiful': 'đẹp tuyệt vời', 'dep': 'đẹp', 'chs': 'chơi', 'rât': 'rất','thất':'thất',
+    'xau': 'xấu', 'sấu': 'xấu', 'ncl': 'nói chung là','tkj': 'thì', 'nưa': 'nữa',
+    'wf': 'wifi', 'ôk': 'ok','đt': 'điện thoại', 'cam': 'màn hình', 'maxloa': 'âm thanh to', 
+    'CTKM': 'chương trình khuyến mãi', 'TGDD': 'thế giới di động', 'zìa': 'về', 
+    'rraats': 'rất', 'mk' : 'mật khẩu', 'vn' : 'việt nam', 'noi':'nói', 'la':'là', 'qtrinh':'quá trình', 'sd':'sử dụng', 
+    'Ms':'mới','vd':'ví dụ','lag':'tệ', 'call':'gọi', 
+    'hên':'tốt', 'tậu':'mua', 'loạn':'tệ', 'đơ':'tệ','dag':'đang', 'sax':'sạc', 'web':'trang mạng', 'ảo': 'tệ',
+    'thik': 'thích', 'iu': 'yêu', 'fake': 'giả mạo', 
+    'quickly': 'nhanh', 'quick': 'nhanh', 'fast': 'nhanh',
+    'fresh': 'tươi', 'delicious': 'ngon',
+
+    'dt': 'điện thoại', 'fb': 'facebook', 'face': 'facebook', 'ks': 'khách sạn', 'nv': 'nhân viên',
+    'nt': 'nhắn tin', 'ib': 'nhắn tin', 'tl': 'trả lời', 'trl': 'trả lời', 'rep': 'trả lời',
+    'fback': 'feedback', 'fedback': 'feedback',
+    'sd': 'sử dụng', 'sài': 'xài', 'dỏm':'tệ', 'xạc': 'sạc',
+
+    '^_^': 'tích cực', ':)': 'tiêu cực', ':(': 'tiêu cực',
+    '❤️': 'tích cực', '👍': 'tích cực', '🎉': 'tích cực', '😀': 'tích cực', '😍': 'tích cực', '😂': 'tích cực', '🤗': 'tích cực', '😙': 'tích cực', '🙂': 'tích cực', 
+    '😔': 'tiêu cực', '😓': 'tiêu cực', 
+    '⭐': 'star', '*': 'star', '🌟': 'star', 
+    'beautiful': u' đẹp tuyệt vời ', u' tl ': u' trả lời ', u' r ': u' rồi ', u' shopE ': u' cửa hàng ',u' order ': u' đặt hàng ',
+    'chất lg': u' chất lượng ',u' sd ': u' sử dụng ',u' dt ': u' điện thoại ',u' nt ': u' nhắn tin ',u' tl ': u' trả lời ',u' sài ': u' xài ',u'bjo':u' bao giờ ',
+    'thik': u' thích ',u' sop ': u' cửa hàng ', ' fb ': ' facebook ', ' face ': ' facebook ', ' very ': u' rất ',u'quả ng ':u' quảng  ',
+    'dep': u' đẹp ',u' xau ': u' xấu ','delicious': u' ngon ', u'hàg': u' hàng ', u'qủa': u' quả ','bk':'biết',
+    'iu': u' yêu ','fake': u' giả mạo ', 'trl': 'trả lời', '><': u' tích cực ', 'dthoai':'điện thoại','thegioididong':'thế giới di động', 'đt': 'điện thoại','dt': 'điện thoại',
+    ' por ': u' tiêu cực ',' poor ': u' tiêu cực ', 'ib':u' nhắn tin ', 'rep':u' trả lời ',u'fback':' feedback ','fedback':' feedback ','zin':'tích cực','fb': 'mạng xã hội','lag':'tiêu cực',
+    'hazzz': 'tiêu cực','test': 'thử','bit': 'biết', 'ak':'à','noiz':'nói','rats':'rất','j':'gì','diss':'tiêu cực',
+    '4 sao': 'tích cực','5 sao': 'tích cực','1 sao': 'tiêu cực', 'phên': 'tích cực','kog': 'không', 'mia': 'mua', 'ja':'giá','way':'quay', 'zay':'vậy'}
+
+
+
+
