3a087153f502ac70f99e6ae117cdbb3a806442ea
leanhtu1808 <kinglat18082003@gmail.com>
2024-04-12 14:56:28 +0700

    feat: add tf dataset

--- utils/variables.py
+++ utils/variables.py
@@ -1,6 +1,5 @@
 from datasets import load_dataset
 from utils.tokenizer import call_tokenizer, tokenize_function,read_csv, make_outputs
-from utils.tf_format import to_tensorflow_format, preprocess_tokenized_dataset
 from utils.config import BATCH_SIZE, MODEL_PATH, TRAIN_PATH, TEST_PATH, VAL_PATH
 import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
@@ -8,6 +7,7 @@ import tensorflow as tf
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from tensorflow.train import CheckpointOptions
 import pandas as pd
+from utils.tf_format import preprocess_tokenized_dataset
 
 raw_datasets = load_dataset('csv', data_files={'train': TRAIN_PATH, 'val': VAL_PATH, 'test': TEST_PATH})
 
@@ -26,6 +26,10 @@ y_test = make_outputs(df_test)
 tokenizer = call_tokenizer()
 
 tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+
+train_tf_dataset = preprocess_tokenized_dataset(tokenized_datasets['train'], tokenizer, y_train, BATCH_SIZE, shuffle=True)
+val_tf_dataset = preprocess_tokenized_dataset(tokenized_datasets['val'], tokenizer, y_val, BATCH_SIZE)
+test_tf_dataset = preprocess_tokenized_dataset(tokenized_datasets['test'],  tokenizer, y_test, BATCH_SIZE)
 
 early_stopping = EarlyStopping(monitor='val_loss',patience=1, verbose=1)
 checkpoint_filepath = MODEL_PATH + '/best.h5'
