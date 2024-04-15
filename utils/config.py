dfe11b250d12a8abb264b81614cdff02c1da96be
leanhtu1808 <kinglat18082003@gmail.com>
2024-04-15 15:47:34 +0700

    refactor: change hyperparam

--- utils/config.py
+++ utils/config.py
@@ -2,7 +2,7 @@ MODEL_PATH = 'model'
 
 MAX_SEQUENCE_LENGTH = 256
 
-BATCH_SIZE = 10
+BATCH_SIZE = 20
 
 EPOCHS = 5
 
@@ -16,8 +16,8 @@ TRAIN_PATH = "./data/Train_without_OTHER.csv"
 VAL_PATH = "./data/Val_without_OTHER.csv"
 TEST_PATH = "./data/Test_without_OTHER.csv"
 
-DROP_OUT = 0.3
+DROP_OUT = 0.2
 
 LEARNING_RATE=1e-4
 
-WEIGHT_DECAY = 5e-3
+WEIGHT_DECAY =5e-3
