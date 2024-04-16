a35dd67079370ee2447eb74f3e95669a27e531d5
leanhtu1808 <kinglat18082003@gmail.com>
2024-04-16 10:10:40 +0700

    ...

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
-from utils.config import BATCH_SIZE
-from predict import reloaded_model, predict_test, replacements, categories, print_acsa_pred_test
-from utils.variables import test_tf_dataset
+# from utils.config import BATCH_SIZE
+# from predict import reloaded_model, predict_test, replacements, categories, print_acsa_pred_test
+# from utils.variables import test_tf_dataset
 
-y_pred = predict_test(reloaded_model, test_tf_dataset, BATCH_SIZE, verbose=1)
-reloaded_model.evaluate(test_tf_dataset, batch_size=BATCH_SIZE, verbose=1)
+# y_pred = predict_test(reloaded_model, test_tf_dataset, BATCH_SIZE, verbose=1)
+# reloaded_model.evaluate(test_tf_dataset, batch_size=BATCH_SIZE, verbose=1)
 
-print_acsa_pred_test(replacements, categories, y_pred[0])
+# print_acsa_pred_test(replacements, categories, y_pred[0])
+
+
+
