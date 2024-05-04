e3596ed109b6f597eb17f08bc7e8f9a6a95fa92d
leanhtu1808 <kinglat18082003@gmail.com>
2024-05-04 17:03:21 +0700

    feat: evaluate for fields

---
+++ test/eval_hotel.py
@@ -0,0 +1,32 @@
+import sys
+# adding Folder_2 to the system path
+sys.path.insert(0, 'utils/')
+from tensorflow.keras.models import load_model
+from create_model import create_model_hotel
+from transformers import TFAutoModel
+from tokenizer import PRETRAINED_MODEL
+from variables_hotel import df_test_hotel, test_hotel_dataset
+from config import MODEL_PATH, BATCH_SIZE
+import numpy as np
+
+replacements = {0: None, 1: 'positive', 2: 'negative', 3: 'neutral'}
+categories = df_test_hotel.columns[1:]
+# Predict on test dataset
+def print_acsa_pred_test(replacements, categories, sentence_pred):
+    sentiments = map(lambda x: replacements[x], sentence_pred)
+    for category, sentiment in zip(categories, sentiments):
+        if sentiment: print(f'=> {category},{sentiment}')
+def predict_test(model, inputs, batch_size=1, verbose=0):
+    y_pred = model.predict(inputs, batch_size=batch_size, verbose=verbose)
+    y_pred = y_pred.reshape(len(y_pred), -1, 4)
+    return np.argmax(y_pred, axis=-1) # sentiment values (position that have max value)
+
+pretrained_bert = TFAutoModel.from_pretrained(PRETRAINED_MODEL, output_hidden_states=True)
+reloaded_model = create_model_hotel(pretrained_bert)
+reloaded_model.load_weights(f'{MODEL_PATH}/hotel.h5')
+y_pred = predict_test(reloaded_model, test_hotel_dataset, BATCH_SIZE, verbose=1)
+reloaded_model.evaluate(test_hotel_dataset  , batch_size=BATCH_SIZE, verbose=1)
+
+print_acsa_pred_test(replacements, categories, y_pred[0])
+
+
