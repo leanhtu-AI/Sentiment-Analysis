33d4bb2e62465ea77c2bfb71551c3651fe58d49b
leanhtu1808 <kinglat18082003@gmail.com>
2024-05-09 14:56:58 +0700

    fix: unused library

--- create_model.py
+++ create_model.py
@@ -1,11 +1,9 @@
+from keras.layers import Dense, Dropout, Input, Layer, concatenate
 from keras.models import Model
-from keras.layers import Input, Dense, Dropout, concatenate
-from keras.layers import Layer
 from tensorflow.keras.optimizers import AdamW
-from transformers import TFAutoModel
-from utils.variables import STEPS_PER_EPOCH, VALIDATION_STEPS, df_train,val_tf_dataset,train_tf_dataset
-from utils.config import MAX_SEQUENCE_LENGTH, BATCH_SIZE, EPOCHS, WEIGHT_DECAY, LEARNING_RATE, DROP_OUT
-from utils.tokenizer import PRETRAINED_MODEL
+
+from utils.config import DROP_OUT, LEARNING_RATE, MAX_SEQUENCE_LENGTH, WEIGHT_DECAY
+from utils.variables import df_train
 
 
 class CustomLayer(Layer):
